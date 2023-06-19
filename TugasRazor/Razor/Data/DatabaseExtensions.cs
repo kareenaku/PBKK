ad8deb78b8f03cb98b6b7b82be42f0119ffcb425
kareenaku <126055373+kareenaku@users.noreply.github.com>
2023-06-19 13:11:32 +0700

    Add files via upload

---
+++ TugasRazor/Razor/Data/DatabaseExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Razor.Data
+{
+    public static class DatabaseExtensions
+    {
+        public static void EnsureDatabaseCreated(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<IssueDbContext>();
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ TugasRazor/Razor/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razor.Models;
+
+namespace Razor.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
