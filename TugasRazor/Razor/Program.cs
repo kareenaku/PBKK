fbcf7d99659a73a306e8f8ce7be3c55d1d679879
kareenaku <126055373+kareenaku@users.noreply.github.com>
2023-06-19 13:16:27 +0700

    Add files via upload

---
+++ TugasRazor/Razor/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Razor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<IssueDbContext>(o => o.UseSqlite("filename=Data/Database/Issue.db"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.EnsureDatabaseCreated();
+app.Run();
